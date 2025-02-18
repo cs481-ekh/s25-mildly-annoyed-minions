b6894d64a241a562ba520cf9b90dc2edaf7b9f75
cjk-bsu <carsonkeller@u.boisestate.edu>
2025-02-17 18:30:55 -0700

    Added functionality to manually select PDF files

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+
 
 class GUI:
     def __init__(self, master):
@@ -6,5 +8,13 @@ class GUI:
         self.master.title("R&D Labs Directory Parser")
         self.master.geometry("500x500")
 
-        self.label = tk.Label(self.master, text="Test Content")
-        self.label.pack(pady=20)
+        self.file_frame = tk.LabelFrame(master, text="Select Files to Parse", padx=20, pady=20)
+        self.file_frame.pack(padx=10, pady=10, fill="x")
+
+        self.select_button = tk.Button(self.file_frame, text="Select Files", command=self.manual_file_select)
+        self.select_button.pack()
+
+    def manual_file_select(self):
+        file_paths = filedialog.askopenfilenames(title="Select Files", filetypes=[("PDF files", "*.pdf")])
+        if file_paths:
+            print("Selected files:", file_paths)
