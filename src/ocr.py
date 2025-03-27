119e3f5c357253eea8db9e787face49fa53493ae
Dylan-Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2025-03-27 00:23:06 -0600

    Start merging

--- src/ocr.py
+++ src/ocr.py
@@ -5,6 +5,8 @@ import subprocess
 import tempfile
 import time
 
+import cv2
+import numpy as np
 import pytesseract
 from PIL import Image, ImageSequence
 from pdf2image import convert_from_path
@@ -19,6 +21,9 @@ class OCRProcessor:
     """Class to handle OCR operations."""
 
     def __init__(self, master):
+        self.WHITELIST = """ !\\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]`abcdefghijklmnopqrstuvwxyz{|}"""
+        self.BLACKLIST = """~_^"""
+        self.pyt_config = f"--psm 6 -c tessedit_char_whitelist={self.WHITELIST} -c tessedit_char_blacklist={self.BLACKLIST}"
         self.master = master
         self.temp_dir = tempfile.gettempdir()
         self.test_images_no_split = [
@@ -140,7 +145,7 @@ class OCRProcessor:
                 pdf_path,
                 grayscale=True,
                 fmt='tiff',
-                dpi=300,
+                dpi=500,
             )
 
             # Save TIFF to temp directory to avoid permission issues
@@ -193,6 +198,48 @@ class OCRProcessor:
         except Exception:
             self.master.gui.handle_error("File Handling Error", f"Unable to open file: {pdf_path}")
             return None
+
+    @staticmethod
+    def sharpen_image(image):
+        sharpen_kernel = np.array([
+            [-1, -1, -1],
+            [-1,  9, -1],
+            [-1, -1, -1]
+        ])
+
+        return cv2.filter2D(image, -1, sharpen_kernel)
+
+    @staticmethod
+    def split_page(image):
+        height, width, _ = image.shape
+        middle = (width // 2)
+
+        left_col = image[:-100, 125:middle]
+        right_col = image[:-100, middle:-110]
+
+        return left_col, right_col
+
+    def process_half(self, image):
+        # Grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Binarization
+        ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+        # Draw the fake-boxes
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (200, 20))
+        dilation = cv2.dilate(thresh, rect_kernel, iterations=1)
+
+        # Draw the bounding boxes based on the fake ones
+        image2 = image.copy()
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(image2, contours, -1, (0, 255, 0), 3)
+
+        # Return the boxed image
+        return image2
+
+    def extract_text_from_processed_image(self, image):
+        return pytesseract.image_to_string(image, lang="eng", config=self.pyt_config).replace("|", "1")
 
     def clean_text(self, extracted_text):
         """Cleans text into the expected format.
